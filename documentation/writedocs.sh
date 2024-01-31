bd1d6476ffd37cc1b291ad20b8126d3930f9c765
Maxime Robeyns <dev@maximerobeyns.com>
2024-01-31 22:39:02 +0000

    Move documentation in pages branch

--- documentation/writedocs.sh
+++ documentation/writedocs.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sphinx-autobuild docs/source docs/build/html --open-browser --port 0 --watch $(dirname "$(pwd)"/bayesian_lora)
+sphinx-autobuild documentation/source documentation/build/html --open-browser --port 0 --watch $(dirname "$(pwd)"/bayesian_lora)
